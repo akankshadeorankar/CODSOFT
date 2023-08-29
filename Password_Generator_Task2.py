3ddfdd506df143c3c2c4672ed51e037556653623
akankshadeorankar <113322073+akankshadeorankar@users.noreply.github.com>
2023-08-29 10:29:55 +0530

    Add files via upload

---
+++ Password_Generator_Task2.py
@@ -0,0 +1,66 @@
+#Password Generator 
+
+from tkinter import *  
+import string 
+import random 
+import pyperclip
+
+def generator():
+    small_alphabets=string.ascii_lowercase
+    capital_alphabets =string.ascii_uppercase
+    numbers=string.digits 
+    special_characters=string.punctuation 
+    
+    all=small_alphabets + capital_alphabets+numbers +special_characters
+    password_length = int(length_box.get())
+    
+    if choice.get()==1:
+        passwordField.insert(0,random.sample(small_alphabets,password_length))
+    
+    if choice.get()==2:
+        passwordField.insert(0,random.sample(small_alphabets+capital_alphabets,password_length))    
+    
+    if choice.get()==3:
+        passwordField.insert(0,random.sample(all,password_length))
+    # password=random.sample(all,password_length)
+    # passwordField.insert(0,password)
+    
+def copy(): 
+    random_password=passwordField.get()   
+    pyperclip.copy(random_password)
+
+root =Tk()
+root.config(bg='gray20')
+choice=IntVar()
+Font=('arial',13,'bold')
+
+passwordLabel =Label(root, text='PASSWORD GENERATOR',font=('Times ',20,'bold italic'),bg='gray20',fg='white')
+passwordLabel.grid(pady=10)#this grid method helps to get text on screen 
+
+weakradioButton=Radiobutton(root,text ='Weak', value=1,variable=choice)
+weakradioButton.grid(pady=5)
+
+MediumradioButton=Radiobutton(root,text ='Medium', value=2,variable=choice)
+MediumradioButton.grid(pady=5)
+
+StrongradioButton=Radiobutton(root,text ='Strong', value=3,variable=choice)
+StrongradioButton.grid(pady=5)
+
+
+lengthLabel =Label(root, text='Password length',font=Font,bg='gray20',fg='white')
+lengthLabel.grid(pady=5)
+
+length_box=Spinbox(root , from_=5,to_=18,width=5,font=Font)
+length_box.grid(pady=5)
+
+generateButton=Button(root , text='Generate' , font=Font , command=generator)
+generateButton.grid(pady=5)
+
+passwordField = Entry(root , width=25 , bd=2 ,font=Font)
+passwordField.grid()
+
+CopyButton=Button(root , text='Copy' , font=Font,command=copy)
+CopyButton.grid(pady=5)
+
+root.mainloop()
+
